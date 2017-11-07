c7eb392cdce405717ae025ff5bcf4c97e6660e72
Giolegrand <g.le.grand@hotmail.com>
2017-11-07 19:04:49 +0100

    change password to better one

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -75,7 +75,7 @@ class AuthenticationController extends Controller
         $afdelingen = $em->getRepository("App:Afdeling")->findByActive(true);
 
         $user = new \App\Entity\Medewerker();
-        $pass = $encoder->encodePassword($user, "test123");
+        $pass = $encoder->encodePassword($user, "changeme");
 
         return $this->render('Authentication/wachtwoord_vergeten.html.twig', array(
             "pass" => $pass,
