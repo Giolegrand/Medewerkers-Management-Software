0ae40f3762e9b6326906588eacbe7ec48b2651db
Giovanni Le Grand <g.le.grand@hotmail.com>
2017-11-09 10:06:19 +0100

    add GebruikerController with account route

---
+++ src/Controller/GebruikerController.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Afdeling;
+
+class GebruikerController extends Controller {
+	/**
+	 *
+	 * @Route("/account", name="account")
+	 */
+	public function accountAction(Request $r){
+		
+	}
+}
