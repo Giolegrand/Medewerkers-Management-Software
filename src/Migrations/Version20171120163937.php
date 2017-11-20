29c824eea62b2a253e327996d28b523a16859d36
Giolegrand <g.le.grand@hotmail.com>
2017-11-20 17:49:35 +0100

    fix medewerker_id

--- src/Migrations/Version20171120163937.php
+++ src/Migrations/Version20171120163937.php
@@ -15,9 +15,8 @@ class Version20171120163937 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE gebruikers ADD medewerker_id INT DEFAULT NULL, CHANGE role role LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\'');
+        $this->addSql('ALTER TABLE gebruikers CHANGE role role LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\'');
         $this->addSql('ALTER TABLE gebruikers ADD CONSTRAINT FK_968F1E253D707F64 FOREIGN KEY (medewerker_id) REFERENCES medewerkers (id)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_968F1E253D707F64 ON gebruikers (medewerker_id)');
     }
 
     public function down(Schema $schema)
