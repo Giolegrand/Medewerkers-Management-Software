1c41228ed0e7f7491254bd0aa3a104e73df7b552
Giolegrand <g.le.grand@hotmail.com>
2017-11-20 18:14:38 +0100

    add voter logic to deny access

---
+++ src/Security/MainVoter.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Security;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+
+class MainVoter extends VoterInterface {
+	protected roleHierarchy;
+
+	public function __construct($roleHierarchy)
+    {  
+        $this->roleHierarchy = $roleHierarchy;
+    }
+
+    protected function hasRole($token, $targetRole){
+    	$reachableRoles = $this->roleHierarchy->getReachableRoles($token->getRoles());
+    	foreach($reachableRoles as $role){
+    		if($role->getRole() == $"")
+    	}
+    }
+
+	protected function supports($attribute, $subject){
+		return true;
+	}
+
+	protected function voteOnAttribute($attribute, $subject, TokenInterface $token){
+		$user = $token->getUser();
+
+		foreach($user->getRoles() as $role){
+			if(preg_match("/\-(.+)/", $role, $r)){
+				if($r[1] === $attribute)
+					return false;
+			}
+		}
+		return true;
+	}
+}
